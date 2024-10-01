85dfe2532b6a216a94e366d4ac312c5f21258b8f
David Greenland <David.Greenland@brighthr.com>
2024-10-01 13:40:23 +0100

    YLW-3119: Practice using named arguments.

--- TheEnchantedLibrary/Program.cs
+++ TheEnchantedLibrary/Program.cs
@@ -1,5 +1,5 @@
 ﻿using TheEnchantedLibrary.Models;
 
-var book1 = new Book("Dusty Old Book", "Ernesto Basile", "Grandidiliomostrosous");
+var book1 = new Book(spell: "Grandidiliomostrosous", title: "Dusty Old Book", author: "Ernesto Basile");
 var book2 = new Book("Cricket Almanac 1924", "Rudolf Dassler", "Owzatamillionwicket");
 Console.WriteLine(book2.Id); // 2
