998f6a6dc7985e6ef842828a13c05b12eccb4c2b
David Greenland <David.Greenland@brighthr.com>
2024-10-02 15:12:41 +0100

    YLW-3119: formatting

--- TheEnchantedLibrary/Models/LibraryConsoleUserInteraction.cs
+++ TheEnchantedLibrary/Models/LibraryConsoleUserInteraction.cs
@@ -5,9 +5,11 @@ public class LibraryConsoleUserInteraction : ILibraryUserInteraction
     public void PrintBooks(Library library)
     {
         Console.WriteLine($"Here is a list of the books in the library");
-        var tableHeader = String.Format("{0,4} | {1,-40} | {2,-20} | {3}", "ID", "Title", "Author", "Spell");
+        var tableHeader = String.Format("{0,4} | {1,-40} | {2,-20} | {3}", 
+            "ID", "Title", "Author", "Spell");
         Console.WriteLine(tableHeader);
-        Console.WriteLine(String.Format("{0,4} | {1,-40} | {2,-20} | {3}", new string('-', 4), new string('-', 40), new string('-', 20), new string('-', 30)));
+        Console.WriteLine(String.Format("{0,4} | {1,-40} | {2,-20} | {3}", 
+            new string('-', 4), new string('-', 40), new string('-', 20), new string('-', 30)));
         foreach (var book in library.Catalogue)
         {
             var tableRow = String.Format("{0,4} | {1,-40} | {2,-20} | {3}", book.Id, book.Title,  book.Author, book.Spell);
