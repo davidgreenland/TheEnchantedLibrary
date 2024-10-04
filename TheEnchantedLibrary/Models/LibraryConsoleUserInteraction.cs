f95125faa85645e766567d7f13022f12e5ce1252
David Greenland <David.Greenland@brighthr.com>
2024-10-04 14:27:56 +0100

    YLW-3119-4 refactor UI table

--- TheEnchantedLibrary/Models/LibraryConsoleUserInteraction.cs
+++ TheEnchantedLibrary/Models/LibraryConsoleUserInteraction.cs
@@ -31,18 +31,21 @@ public class LibraryConsoleUserInteraction : ILibraryUserInteraction
 
     public void PrintBooks(ICollection<Book> books)
     {
-        var tableHeader = String.Format("{0,4} | {1,-40} | {2,-20} | {3}", "ID", "Title", "Author", "Spell");
-
         Console.WriteLine($"Here is a list of the books in the library");
-        Console.WriteLine(tableHeader);
-        Console.WriteLine(String.Format("{0,4} | {1,-40} | {2,-20} | {3}", 
-            new string('-', 4), new string('-', 40), new string('-', 20), new string('-', 30)));
+        PrintTableHeader();
 
         foreach (var book in books)
         {
             PrintBook(book);
         }
         Console.WriteLine();
+    }
+
+    private void PrintTableHeader()
+    {
+        Console.WriteLine(String.Format("{0,4} | {1,-40} | {2,-20} | {3}", "ID", "Title", "Author", "Spell"));
+        Console.WriteLine(String.Format("{0,4} | {1,-40} | {2,-20} | {3}",
+            new string('-', 4), new string('-', 40), new string('-', 20), new string('-', 30)));
     }
 
     private void PrintBook(Book book) 
---
+++ TheEnchantedLibrary/Models/Search.cs
@@ -0,0 +1,19 @@
+﻿namespace TheEnchantedLibrary.Models;
+
+public class Search: LibraryAction
+{
+    public override string Message { get; } = "E[x]it application" + Environment.NewLine;
+
+    private readonly ILibraryUserInteraction _libraryUserInteraction;
+
+    public Search(ILibraryUserInteraction libraryUserInteraction)
+    {
+
+        _libraryUserInteraction = libraryUserInteraction;
+    }
+
+    public override void Execute(ILibrary library)
+    {
+        _libraryUserInteraction.PrintMessage("|| Goodbye. :-) ||");
+    }
+}
